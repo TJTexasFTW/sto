373512efa2b458a2026eff71f10ab080228cd71e
Theresa Stocking <nottodayjose@MacBook-Pro.attlocal.net>
2019-04-27 23:02:04 -0500

    Goodnight

--- src/Components/EmployeeUpdate/EmployeeUpdate.js
+++ src/Components/EmployeeUpdate/EmployeeUpdate.js
@@ -1,6 +1,6 @@
-import React, {Component} from './node_modules/react';
-import {Link, Redirect} from './node_modules/react-router-dom'
-import axios from './node_modules/axios';
+import React, {Component} from 'react';
+import {Link, Redirect} from 'react-router-dom'
+
 
 class Employee_Update_Delete extends Component {
     constructor() {
--- src/Components/Main/Home.js
+++ src/Components/Main/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Link, Redirect} from 'react-router-dom'
 import axios from 'axios';
+import './Home.css';
 
 class Home extends Component {
     constructor() {
@@ -15,13 +16,17 @@ class Home extends Component {
     render() {
 
         return(
+            <body>
             <div className="Home">
                 SCHEDULED TIME OFF
-            
-            <p className="current_month">CURRENT MONTH</p>
-            <p className="next_month">NEXT MONTH</p>
-            <p className="two_months_out">TWO MONTHS OUT</p>
+            {/* <img className = "laid_back" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA7J4UCmxeP-lg1Qx5I6EGDxQXk2x6xxNTGM3Xw35plmClwxvQGg"/> */}
+            <div className="months">
+                <p className="current_month">CURRENT MONTH</p>
+                <p className="next_month">NEXT MONTH</p>
+                <p className="two_months_out">TWO MONTHS OUT</p>
             </div>
+            </div>
+            </body>
         )
     }
 }
