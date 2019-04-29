11f594091bce0fcf26d8add9ff7bd5c0c0827374
Theresa Stocking <nottodayjose@MacBook-Pro.local>
2019-04-29 12:25:13 -0500

    This and next month working!!!

--- src/Components/Main/Home.js
+++ src/Components/Main/Home.js
@@ -101,6 +101,67 @@ let dateEntry = [];
                 }
         }
     
+        let infoNext = this.state.nextMonth;
+        // console.log("This should be info from database: ", info);
+        let dateEntryNext = [];
+                
+                for (let i = 0; i < infoNext.length; i++) {
+        
+                    switch(infoNext[i].from_table) {
+                        case 'BLOCKED': 
+                        if (String(infoNext[i].start_date.substring(5,5)) == 0) {
+                            var start = infoNext[i].start_date.substring(6,10);
+                            console.log("Trying to get rid of leading zero");
+                        } else {
+                            start = infoNext[i].start_date.substring(5,10)
+                        }
+                        dateEntryNext.push(  
+                            <p>{start} RESTRICTED</p>);
+                            // console.log("***We are in the makeList switch statement***");
+                            break;
+                        case 'STO': 
+        
+                            if (String(infoNext[i].start_date.substring(5,5)) == 0) {
+                                var start = infoNext[i].start_date.substring(6,10);
+                                console.log("Trying to get rid of leading zero");
+                            } else {
+                                start = infoNext[i].start_date.substring(5,10)
+                            }
+        
+                            if (String(infoNext[i].end_date.substring(5,5)) == 0) {
+                                var end = infoNext[i].end_date.substring(6,10);
+                                console.log("Trying to get rid of leading zero");
+                            } else {
+                                end = infoNext[i].end_date.substring(5,10)
+                            }
+                            //check if start and end date match
+                            if (infoNext[i].start_date === infoNext[i].end_date) {
+                            dateEntryNext.push(  
+                            <p>{start} {infoNext[i].initials}</p>);
+                            } else {
+                                //end date is different - concantenate start & end
+                                dateEntryNext.push(  
+                                    <p>{start} thru {end} {infoNext[i].initials}</p>);
+                            }
+                            break;
+                        case 'EVENT':
+        
+                        if (String(infoNext[i].start_date.substring(5,5)) == 0) {
+                            var start = infoNext[i].start_date.substring(6,10);
+                            console.log("Trying to get rid of leading zero");
+                        } else {
+                            start = infoNext[i].start_date.substring(5,10)
+                        }
+        
+                        dateEntryNext.push(  
+                            <p>{start} {infoNext[i].initials}</p>);
+                            console.log("***We are in the makeList switch statement***");
+                            break;
+                        default: 
+                          console.log("Home Component - issue in render area");
+                        }
+                }
+
         // makeList(info);
 
         return(
@@ -113,8 +174,9 @@ let dateEntry = [];
                         </ul> 
                     </div>
                     <div className="next_month"><u>NEXT MONTH</u>
-                       <div className='data'>
-                       </div> 
+                    <ul className='sto_listing'>
+                            {dateEntryNext}
+                        </ul> 
                     </div>
                     
                     <div className="two_months_out"><u>IN 3 MONTHS</u></div>
