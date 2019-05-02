ac9b3de3d30b12a2284147c91ea983f93c91b159
Theresa Stocking <nottodayjose@MacBook-Pro.local>
2019-05-02 16:51:01 -0500

    May 2 450 Packing Up to Go Home

--- src/Components/EmployeeUpdate/EmployeeUpdate.js
+++ src/Components/EmployeeUpdate/EmployeeUpdate.js
@@ -19,9 +19,11 @@ class Employee_Update_Delete extends Component {
                 <h1 className = 'appHeading'>SCHEDULED TIME OFF (STO)</h1>
                 <h2 className = 'subHeading'>Update / De-activate Employee</h2>
 
-            <p className='inputLabel'>Name:  <input className='inputBox' placeholder = "FLast"/></p>
-            <p className='inputLabel'>Initials:  <input className='inputBox' placeholder = "Initials"/></p>
-            <p className='inputLabel'>Password:  <input className='inputBox' placeholder = "Password" type="password"/></p>
+            <p className='inputLabel'>Current Name:  <input className='inputBox' placeholder = "FLast"/></p>
+            <p className='inputLabel'>Revised Name:  <input className='inputBox' placeholder = "FLast"/></p>
+            <p className='inputLabel'>Current Initials:  <input className='inputBox' placeholder = "Initials"/></p>
+            <p className='inputLabel'>Revised Initials:  <input className='inputBox' placeholder = "Initials"/></p>
+            {/* <p className='inputLabel'>Password:  <input className='inputBox' placeholder = "Password" type="password"/></p> */}
  
             <div class="Administrative">
                 <label className = 'adminCheckbox' for="adminChk">Administrator: </label>
@@ -38,7 +40,7 @@ class Employee_Update_Delete extends Component {
             <div className="button_choices">
                 <Link to='/'><button className = "adminButton">HOME</button></Link>
                 <Link to='/employee_maintenance'><button className = "adminButton">EMPLOYEE MAINT MENU</button></Link>
-                <Link to='/'><button className = "adminButton">LOG OFF</button></Link>
+                {/* <Link to='/'><button className = "adminButton">LOG OFF</button></Link> */}
                 <button onClick={this.handleSubmit} className="adminButton">SUBMIT</button>
                     
                 </div>
